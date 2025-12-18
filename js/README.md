85d6bd9c199ec395f213d4f8c7d5c35547776c9c
Yandry Villagomez <rafaeldev.villagomez@gmail.com>
2025-12-18 13:52:09 +0100

    New feature : [magic_inc] funcion en vanilla js que incrementa o decrementa magicamente un numero dado

---
+++ js/README.md
@@ -0,0 +1,18 @@
+# Magic Inc/Dec number🪄
+
+Esta es una utilidad de JavaScript que implementa una lógica de incremento y decremento "mágica".Esta función opera sobre el **primer dígito significativo** y ajusta el **orden de magnitud** (exponente) del número de forma automática.
+
+## 📋 Requisitos
+
+* **Node.js**: Versión 10.4.0 o superior (necesaria para el soporte nativo de `BigInt`).
+
+## 🚀 Ejecución rápida
+
+Para ejecutar el script y ver los resultados por consola, sigue estos pasos:
+
+1. Posicionate dentro de la carpeta utils donde esta nuestra función magic_inc dentro del archivo llamado `index.js`.
+2. Abre tu terminal o consola de comandos.
+3. Ejecuta el siguiente comando :
+
+```bash
+node index.js
---
+++ js/utils/index.js
@@ -0,0 +1,77 @@
+
+/***
+ * Magic Inc/Dec
+ * 
+ * Implementa una función magic_inc(value, mode) que incremente o decremente "mágicamente" un número dado.
+ * @param {number|bigint|string} value - El valor a incrementar o decrementar.
+ * @param {string} mode - El modo de operación: 'inc' para incrementar, 'dec' para decrementar.
+ * @returns {number|bigint} - El valor incrementado o decrementado.
+ */
+function magic_inc() {
+    let [value, mode] = arguments;
+
+    // Validación: Cualquier valor no numberérico null o 0 devolvemos 0 
+    let number = parseFloat(value);
+    if (!value || isNaN(number) || number === 0 || Math.sign(number) == -0) return 0;
+
+    const isNegative = Math.sign(number) === -1; // Condicion pasa saber si es un numero negativo
+    const absNumber = Math.abs(number); // Convertimos a positivo para  manipularlo
+    const action = mode.toLowerCase(); // Convertimos el modo a minisculas para evitar errores
+
+    // Extraer exponente y primer dígito (soporta números gigantes/pequeños)
+    let [numberSlice, exp] = absNumber.toExponential().split('e'); // Tranformamos numero a exponencial y posteriormente hacemos un split nos quedamos con la parte  numerica y exponente 
+    let exponent = parseInt(exp); // Convertimos exponente a un entero , no da igual si es positivo o negativo 
+    let firstDigit = parseInt(numberSlice[0]); // Cogemos el primer digito de nuestra parte numerica 
+
+    // Lógica de funcionamiento :
+    // Numero negativo : incrementar un  numero negativo es reducir su valor absoluto y decrementar es aumentar su valor absoluto
+    // Numero positivo : incrementar un numero positivo es aumentar su valor absoluto y decrementar es reducir su valor absoluto
+
+    let shouldIncreaseAbs = isNegative ? action === 'dec' : action === 'inc';
+
+    let resDigit, resExp = exponent;
+
+    if (shouldIncreaseAbs) {
+        // Ejemplo: 0.76 -> firstDigit:7, exp:-1. Resultado: (7+1)*10^-1 = 0.8
+        resDigit = firstDigit + 1;
+        //Logica para manejar primeros digitos mayores a 9 
+        if (resDigit > 9) {
+            resDigit = 1;
+            resExp++;
+        }
+    } else {
+        // Ejemplo: 17 -> d:1, e:1. Como 17 > 10, el siguiente hacia abajo es 10...
+        resDigit = firstDigit - 1;
+        if (resDigit < 1) {
+            resDigit = 9;
+            resExp--;
+        }
+    }
+
+    /*Parte Bonus  */
+    let result;
+    // El exponente debe ser un BigIny o mayor o igual a 15 en su Exp para ser BigInt , los Number  mayores a 15 digitos pierden precision . 
+    // Si no es asi retornamos un Number 
+    if (typeof value === 'bigint' || resExp >= 15) {
+
+        result = BigInt(resDigit) * (10n ** BigInt(resExp));
+        if (isNegative) result = -result;
+        return result;
+    } else {
+      
+        result = resDigit * Math.pow(10, resExp);
+        if (isNegative) result *= -1;
+       
+        return Number(result.toPrecision(1));  // Limpiamos resto de coma flotante
+    }
+
+}
+
+
+console.log(magic_inc(92999999999999999.0, 'dec'));
+console.log(magic_inc(17, 'dec'));
+console.log(magic_inc(-0.5264523, 'inc'));
+console.log(magic_inc("pepe", 'inc'));
+console.log(magic_inc(0, 'inc'));
+console.log(magic_inc(-0, 'inc'));
+console.log(magic_inc(1568.548, 'inc'));
